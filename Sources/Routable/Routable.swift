e33d32fb558cca9f97016d052c6222196a2f2570
Alex Fargo <me@alexfargo.xyz>
2022-11-21 14:16:35 -0800

    add comments to routable

--- Sources/Routable/Routable.swift
+++ Sources/Routable/Routable.swift
@@ -12,6 +12,7 @@ public typealias HTTPHeaders = [String: String]
 public typealias HTTPBody = Data
 public typealias HTTPParameters = [String: String]
 
+/// HTTP request methods
 public enum HTTPMethod: String {
   case get    = "GET"
   case post   = "POST"
@@ -21,9 +22,27 @@ public enum HTTPMethod: String {
 }
 
 public protocol Routable {
+
+  /// The method to use for the request for this route
   var method: HTTPMethod { get }
+  /// The path to use for the URL for this route.
   var path: String? { get }
+  /// Any parameters to put inside the URL for this route.
   var parameters: HTTPParameters? { get }
+  /// Any body data associated with this route.
   var body: HTTPBody? { get }
+  /// Any headers associated with this route.
   var headers: HTTPHeaders? { get }
+
+}
+
+public extension Routable {
+
+  /// Default to nil.
+  var parameters: HTTPParameters? { nil }
+  /// Default to nil.
+  var body: HTTPBody? { nil }
+  /// Default to nil.
+  var headers: HTTPHeaders? { nil }
+
 }
